af86fdfd20ce67bb64333dff1ff1573daf8feff8
湯浅 信彦 <2534721+yuasys@users.noreply.github.com>
2021-08-28 18:59:22 +0900

    initial

---
+++ main/Scripts/jtablesite.js
@@ -0,0 +1,47 @@
+﻿$(document).ready(function() {
+    $('.expandable-header').click(function(e) {
+        e.preventDefault();
+
+        var $header = $(this);
+        var $content = $header.nextAll('.expandable-content');
+
+        if ($content.is(':visible')) {
+            $content.slideUp('fast');
+        } else {
+            $content.slideDown('fast');
+        }
+    });
+
+    SyntaxHighlighter.all();
+
+    $(".tabsContainer").tabs();
+
+    $('.contains-issues')
+        .find('[data-issue]')
+        .each(function() {
+
+            var $this = $(this);
+            var issueIds = $this.attr('data-issue');
+            if (!issueIds) {
+                return;
+            }
+
+            $this.append(' [');
+
+            var issueList = issueIds.split(',');
+            $.each(issueList, function(index, issueId) {
+                if (index > 0) {
+                    $this.append(', ');
+                }
+
+                $('<a />')
+                    .text('#' + issueId)
+                    .attr('href', 'https://github.com/hikalkan/jtable/issues/' + issueId)
+                    .attr('target', '_blank')
+                    .appendTo($this);
+            });
+
+            $this.append(']');
+
+        });
+});
